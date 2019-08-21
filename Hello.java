8709d82a133d840febda7ffa6633e55a1ff42850
zamanhk <54368676+zamanhk@users.noreply.github.com>
2019-08-21 12:00:28 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,22 @@
+/* Java Program
+	Intro Program
+	Hasib Zaman
+	8/21/19
+	255, Section 4
+*/
+
+public class Hello {
+	public static void printHeading(){
+		System.out.println("Hasib Zaman");
+		System.out.println("Java Introduction");
+		System.out.println("Practice with Java");
+		System.out.println("Version 8");
+		System.out.println("004 and Section 4");
+		System.out.println();
+	}
+
+public static void main(String [] args){
+	printHeading();
+	System.out.println("Hello");
+	}
+}
